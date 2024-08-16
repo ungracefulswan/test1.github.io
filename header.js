2ab1cd53a8d05e3c1954ccf5ce645178949e50c5
ungracefulswan <anungracefulswan@gmail.com>
2024-08-16 01:09:30 -0400

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ let appHeader = `
     <nav style="background-color:MidnightBlue">
       <a href=index.html><button>Home</button></a>
       <a  href="test.html"><button>About</button></a>
-      <a  href="blogposts/post1.html"><button>Blog</button></a>
+      <a  href="post1.html"><button>Blog</button></a>
     </nav>
 `;
 document.getElementById("app-header").innerHTML = appHeader;
