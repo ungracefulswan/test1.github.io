23cb132f49330c52bdb65ae1ad75a6647dd2e51d
ungracefulswan <anungracefulswan@gmail.com>
2024-08-16 01:01:47 -0400

    Add files via upload

---
+++ header.js
@@ -0,0 +1,8 @@
+let appHeader = `
+    <nav style="background-color:MidnightBlue">
+      <a href=index.html><button>Home</button></a>
+      <a  href="test.html"><button>About</button></a>
+      <a  href="blogposts/post1.html"><button>Blog</button></a>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
